b3c65b058576a49dedf2082e734a193c86e00eb1
Hector Cortes <hector_aguila01@hotmail.com>
2020-05-06 18:38:22 -0500

    version 1.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+   firebaseConfig : {
+    apiKey: 'AIzaSyCA6mNcw9wFN6oXnnqEVHm5Icd5L3WhuuY',
+    authDomain: 'set99-d751c.firebaseapp.com',
+    databaseURL: 'https://set99-d751c.firebaseio.com',
+    projectId: 'set99-d751c',
+    storageBucket: 'set99-d751c.appspot.com',
+    messagingSenderId: '130951054862',
+    appId: '1:130951054862:web:cc75f06cbaf3990efa9e5f',
+    measurementId: 'G-G50P3F2PS3'
+   }
+  };
 
 /*
  * For easier debugging in development mode, you can import the following file
