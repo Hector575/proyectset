7733ed5acf260e96a0d25dd23026eb6bd19f344b
Hector575 <61898923+Hector575@users.noreply.github.com>
2020-06-03 16:53:50 -0500

    version 22

--- src/app/Registra/busqueda/busqueda.component.ts
+++ src/app/Registra/busqueda/busqueda.component.ts
@@ -1,13 +1,100 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
+import { DataService } from '@app/auth/services/data-db.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
+
 export class BusquedaComponent implements OnInit {
 
-  constructor() { }
+  public contactForm: FormGroup;
+
+  public inmueblesEncontrados = [];
+
+  get caracteristicas() { return this.contactForm.get('caracteristicas') as FormArray; }
+  constructor(
+    private dbData: DataService,
+    private afs: AngularFirestore,
+    private formBuilder: FormBuilder,
+  ) {
+
+    this.contactForm = this.createForm();
+
+  }
+
+  createForm() {
+    return new FormGroup({
+
+      caracteristicas: this.formBuilder.array(
+        this.dbData.getCaracteristicas().map(caracteristica => {
+          return this.formBuilder.group({
+            id: caracteristica.id,
+            descripcion: caracteristica.descripcion,
+            seleccionado: false,
+          })
+        })
+      ),
+
+    });
+  }
+
+
+
+
+  onSubmit(): void {
+
+
+
+    // filtrados tags, es decir, solo rescatamos los que el usuario selecciono
+    const selectedValuesToSearch = this.caracteristicas.value.filter(c => c.seleccionado === true);
+    console.log('selectedValuesToSearch', selectedValuesToSearch);
+
+
+    // hacemos un arreglo vacio, en donde vamos a vaciar los inmuebles encontrados
+
+
+    // vamos a la base de datos y traemos los inmuebles registrados
+    this.afs.collection<any>('ingfor').valueChanges().subscribe(inmueblesEncontrandosEnBD => {
+      console.log('inmueblesEncontrandosEnBD', inmueblesEncontrandosEnBD)
+      this.inmueblesEncontrados = inmueblesEncontrandosEnBD.filter(inmuebleEncontradoEnDB => {
+        let encontrado = false;
+        let contador = 0;
+
+        if (inmuebleEncontradoEnDB.caracteristicas) {
+          for (const inmCaracteristica of inmuebleEncontradoEnDB.caracteristicas) {
+
+
+            for (const selectedValueToSearch of selectedValuesToSearch) {
+              if (inmCaracteristica.id === selectedValueToSearch.id && inmCaracteristica.seleccionado === selectedValueToSearch.seleccionado) {
+                contador++;
+                if (contador == selectedValuesToSearch.length) {
+                  encontrado = true;
+                }
+              }
+            }
+          }
+
+          inmuebleEncontradoEnDB.contador = contador;
+        }
+
+        inmuebleEncontradoEnDB.caracteristicas = inmuebleEncontradoEnDB.caracteristicas.filter(c => c.seleccionado);
+
+        return encontrado;
+      });
+
+
+
+      console.log('inmueblesEncontrados', this.inmueblesEncontrados)
+
+    });
+
+    // console.log(JSON.stringify(this.contactForm.getRawValue()));
+    // console.log(this.contactForm.getRawValue())
+  }
 
   ngOnInit(): void {
   }
--- src/app/auth/services/data-db.service.ts
+++ src/app/auth/services/data-db.service.ts
@@ -9,11 +9,11 @@ import { Clientes } from '@shared/Models/Clientes.interface';
   providedIn: 'root'
 })
 export class DataService {
-  
+
 
   private ingforCollection: AngularFirestoreCollection<MessageI>;
   private etiquetasCollection: AngularFirestoreCollection<Etiquetas>;
-  private ClientesCollection:AngularFirestoreCollection<Clientes>;
+  private ClientesCollection: AngularFirestoreCollection<Clientes>;
 
   constructor(private afs: AngularFirestore) {
     this.ingforCollection = afs.collection<MessageI>('ingfor');
@@ -27,12 +27,88 @@ export class DataService {
   saveEtiqueta(newHabitacional: Etiquetas): void {
     this.etiquetasCollection.add(newHabitacional);
   }
- getInmuebles(email: string){
-   console.log(email)
-   return this.afs.collection<MessageI>('ingfor', ref => ref.where('email', '==', email)).valueChanges();
- }
- saveClientes(newClientes: Clientes): void {
-  this.ClientesCollection.add(newClientes);
+  getInmuebles(email: string) {
+    console.log(email)
+    return this.afs.collection<MessageI>('ingfor', ref => ref.where('email', '==', email)).valueChanges();
+  }
+  saveClientes(newClientes: Clientes): void {
+    this.ClientesCollection.add(newClientes);
+  }
+
+  getCaracteristicas() {
+    return [
+      { id: 1, descripcion: '5k renta' },
+      { id: 2, descripcion: '9k renta' },
+      { id: 3, descripcion: '30k renta' },
+      { id: 4, descripcion: '70k renta' },
+      { id: 5, descripcion: '.15M renta' },
+      { id: 6, descripcion: '.35M renta' },
+      { id: 7, descripcion: '.70M renta' },
+      { id: 8, descripcion: '.90M renta' },
+      { id: 9, descripcion: '.5M venta' },
+      { id: 10, descripcion: '1.1M venta' },
+      { id: 12, descripcion: '2.5M venta' },
+      { id: 13, descripcion: '6M venta' },
+      { id: 14, descripcion: '15M venta' },
+      { id: 15, descripcion: '50M venta' },
+      { id: 16, descripcion: '500M venta' },
+      { id: 17, descripcion: '999M+ venta' },
+      { id: 18, descripcion: '3 baños' },
+      { id: 19, descripcion: '5 baños' },
+      { id: 20, descripcion: '9+ baños' },
+      { id: 21, descripcion: '3 habitaciones' },
+      { id: 22, descripcion: '5 habitaciones' },
+      { id: 23, descripcion: '9+ habitaciones' },
+      { id: 24, descripcion: '$ MX' },
+      { id: 25, descripcion: '$ US' },
+      { id: 26, descripcion: '$ Euro' },
+
+    ]
+  }
+
 }
-  
-}
+
+
+// public createReaction(comment: IComment, reactionType: EReactionType) {
+//   const reaction: IReaction = {
+//     // commentId: comment._id,
+//     userId: this.authService.user.uid,
+//     knowAs: this.authService.user.displayName,
+//     typeId: reactionType,
+//     // created: this.commentsFirebaseService.firebase.firestore.FieldValue.serverTimestamp(),
+//   }
+
+//   this.commentsFirebaseService.db.collection("comments").doc(comment._id).update({
+//     [reactionType === 1 ? 'inFavor' : 'against']: this.commentsFirebaseService.firebase.firestore.FieldValue.increment(1),
+//     reactions: this.commentsFirebaseService.firebase.firestore.FieldValue.arrayUnion(reaction)
+
+//   });
+// }
+
+// public deleteReaction(comment: IComment) {
+//   this.commentsFirebaseService.db.collection("comments").doc(comment._id).update({
+//     [comment._reaction.typeId === 1 ? 'inFavor' : 'against']: this.commentsFirebaseService.firebase.firestore.FieldValue.increment(-1),
+//     reactions: this.commentsFirebaseService.firebase.firestore.FieldValue.arrayRemove(comment._reaction)
+//   }).then(result => {
+//     comment._reaction = undefined;
+//   });
+// }
+
+// public updateReaction(comment: IComment, reactionType: EReactionType) {
+
+//   this.commentsFirebaseService.db.collection("comments").doc(comment._id).update({
+//     [comment._reaction.typeId === 1 ? 'inFavor' : 'against']: this.commentsFirebaseService.firebase.firestore.FieldValue.increment(-1),
+//     reactions: this.commentsFirebaseService.firebase.firestore.FieldValue.arrayRemove(comment._reaction)
+//   }).then(result => {
+//     const reaction: IReaction = {
+//       userId: this.authService.user.uid,
+//       knowAs: this.authService.user.displayName,
+//       typeId: reactionType,
+//     }
+//     comment._reaction = reaction;
+//     this.commentsFirebaseService.db.collection("comments").doc(comment._id).update({
+//       [reactionType === 1 ? 'inFavor' : 'against']: this.commentsFirebaseService.firebase.firestore.FieldValue.increment(1),
+//       reactions: this.commentsFirebaseService.firebase.firestore.FieldValue.arrayUnion(reaction)
+//     });
+//   });
+// }
